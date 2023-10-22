88670a04de956484c44951cb352caf8ecbcf45b8
monal <dnyaneshwarhiwale224@gmail.com>
2023-10-22 14:45:29 +0530

    Commit First

---
+++ src/main/java/QA_Automation_WebApplication/QA_Automation_WebApplication/App.java
@@ -0,0 +1,13 @@
+package QA_Automation_WebApplication.QA_Automation_WebApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/QA_Automation_WebApplication/QA_Automation_WebApplication/AppTest.java
@@ -0,0 +1,20 @@
+package QA_Automation_WebApplication.QA_Automation_WebApplication;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
