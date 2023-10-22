2bfcf7bf97f709de00896930f15984f044f201b3
monal <dnyaneshwarhiwale224@gmail.com>
2023-10-22 18:19:11 +0530

    .

--- src/test/java/stepdefination/Register.java
+++ src/test/java/stepdefination/Register.java
@@ -4,11 +4,11 @@ import java.util.Map;
 
 import org.openqa.selenium.WebDriver;
 
+import factory.DriverFactory;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import pages.HomePage;
-import pages.List;
 import pages.RegisterPage;
 
 public class Register {
@@ -31,39 +31,38 @@ public class Register {
 
 	@When("user click on register button and navigates to register form")
 	public void user_click_on_register_button_and_navigates_to_register_form() throws InterruptedException {
-		
 		homepage.clickOnRegisterButton();
 	}
 	
-	@When("user enter the information in below required field using asMaps")
-	public void user_enter_the_information_in_below_required_field_using_as_maps(DataTable dataTable) throws InterruptedException {
-		List<Map<String, String>> datatable=dataTable.asMaps(String.class, String.class);
-		   for(Map<String, String> datamap:datatable) {
-			   driver= DriverFactory.getDriver();
-			   registerpage=new RegisterPage(driver);
-			   registerpage.enterFirstName(datamap.get("firstname"));
-			   registerpage.enterLastName(datamap.get("lastname"));
-			   registerpage.enterEmail(datamap.get("email"));
-			   registerpage.enterTelephone(datamap.get("telephone"));
-			   registerpage.enterPassword(datamap.get("password"));
-			   registerpage.enterConfirmPassword(datamap.get("password"));
-		   }
-	}
+//	@When("user enter the information in below required field using asMaps")
+//	public void user_enter_the_information_in_below_required_field_using_as_maps(DataTable dataTable) throws InterruptedException {
+//	//	List<Map<String, String>> datatable=dataTable.asMaps(String.class, String.class);
+//		   for(Map<String, String> datamap:datatable) {
+//			   driver= DriverFactory.getDriver();
+//			   registerpage=new RegisterPage(driver);
+//			   registerpage.enterFirstName(datamap.get("firstname"));
+//			   registerpage.enterLastName(datamap.get("lastname"));
+//			   registerpage.enterEmail(datamap.get("email"));
+//			   registerpage.enterTelephone(datamap.get("telephone"));
+//			   registerpage.enterPassword(datamap.get("password"));
+//			   //registerpage.enterConfirmPassword(datamap.get("password"));
+//		   }
+//	}
 
-	@When("user enter the information in below required field using asLists")
-	public void user_enter_the_information_in_below_required_field_using_as_lists(DataTable dataTable) throws InterruptedException {
-	   List<List<String>> dataList=dataTable.asLists(String.class);
-	   
-	   for(List<String> dataL: dataList) {
-		   driver= DriverFactory.getDriver();
-		   registerpage=new RegisterPage(driver);
-		   registerpage.enterFirstName(dataL.get(0));
-		   registerpage.enterLastName(dataL.get(1));
-		   registerpage.enterEmail(dataL.get(2));
-		   registerpage.enterTelephone(dataL.get(3));
-		   registerpage.enterPassword(dataL.get(4));
-		   registerpage.enterConfirmPassword(dataL.get(5));
-		   
-	   }
-
+//	@When("user enter the information in below required field using asLists")
+//	public void user_enter_the_information_in_below_required_field_using_as_lists(DataTable dataTable) throws InterruptedException {
+//	   List<List<String>> dataList=dataTable.asLists(String.class);
+//	   
+//	   for(List<String> dataL: dataList) {
+//		   driver= DriverFactory.getDriver();
+//		   registerpage=new RegisterPage(driver);
+//		   registerpage.enterFirstName(dataL.get(0));
+//		   registerpage.enterLastName(dataL.get(1));
+//		   registerpage.enterEmail(dataL.get(2));
+//		   registerpage.enterTelephone(dataL.get(3));
+//		   registerpage.enterPassword(dataL.get(4));
+//		   registerpage.enterConfirmPassword(dataL.get(5));
+//		   
+//	   }
+//
 }
