62085242f3520b5ffc2b1833b9583561b2aa1393
monal <dnyaneshwarhiwale224@gmail.com>
2023-12-09 10:36:48 +0530

    First Commit Android

---
+++ QA_Automation_AndroidApplication/src/main/java/QA_Automation_AndroidApplication/QA_Automation_AndroidApplication/App.java
@@ -0,0 +1,13 @@
+package QA_Automation_AndroidApplication.QA_Automation_AndroidApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
