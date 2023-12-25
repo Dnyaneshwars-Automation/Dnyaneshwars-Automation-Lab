4bf4363b7e9036e11d9794fab49a39321fc17063
monal <dnyaneshwarhiwale224@gmail.com>
2023-12-25 10:05:16 +0530

    commit to webrepo

--- src/test/java/stepdefination/BaseClass.java
+++ src/test/java/stepdefination/BaseClass.java
@@ -1,31 +1,7 @@
-//package stepdefination;
-//
-//import java.util.concurrent.TimeUnit;
-//
-//import org.openqa.selenium.WebDriver;
-//
-//import factory.DriverFactory;
-//import io.cucumber.java.After;
-//import io.cucumber.java.Before;
-//
-//public class BaseClass {
-//	
-//	
-//	public static WebDriver driver;
-//	
-//	@Before 
-//	public void setUpBrowser() {
-//		DriverFactory.initializebrowser("chrome");
-//		driver=DriverFactory.getDriver();
-//		driver.manage().window().maximize();
-//		driver.manage().deleteAllCookies();
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//		driver.get("https://admin-demo.nopcommerce.com/login");
-//	}
-//	
-//	@After
-//	public void tearDown() {
-//		driver.close();
-//	}
-//
-//}
+package stepdefination;
+
+
+public class BaseClass {
+	
+
+}
